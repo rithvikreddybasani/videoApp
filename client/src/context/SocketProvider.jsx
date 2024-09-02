85e98f9b67b486ae18ce1eeb0368632663f85d6b
rithvik reddy <147538307+rithvikreddybasani@users.noreply.github.com>
2024-09-02 23:02:40 +0530

    Update SocketProvider.jsx

--- client/src/context/SocketProvider.jsx
+++ client/src/context/SocketProvider.jsx
@@ -9,7 +9,7 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io("localhost:8000"), []);
+  const socket = useMemo(() => io("https://videoapp-ie32.onrender.com/"), []);
 
   return (
     <SocketContext.Provider value={socket}>
